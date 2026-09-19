12920e3fe152b1c8f733cd85fd6be06fa019b9f2
wWyo_O <zingert03@gmail.com>
2026-09-19 15:35:47 +0700

    Fix include path for test header file

--- test/unit/core/test_bytes.c
+++ test/unit/core/test_bytes.c
@@ -49,7 +49,7 @@
 
 #include <xury/xury.h>
 #include "core/internal/bytes.h"
-#include "tests/test.h"
+#include "test/test.h"
 
 /*
  * ============================================================================
